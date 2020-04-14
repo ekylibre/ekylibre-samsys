e070feb4457cf136a4a4057a415a7d3661093081
David JOULIN <ionosphere.consulting@gmail.com>
2020-04-14 02:07:11 +0200

    Update methods for creating sensors, equipments and indicators.

--- app/integrations/samsys/samsys_integration.rb
+++ app/integrations/samsys/samsys_integration.rb
@@ -6,7 +6,7 @@ module Samsys
       globals: {
         strip_namespaces: true,
         convert_response_tags_to: ->(tag) { tag.snakecase.to_sym },
-        raise_errors: false
+        raise_errors: true
       },
       locals: {
         advanced_typecasting: true
@@ -22,45 +22,167 @@ module Samsys
     BASE_URL = "https://app.samsys.io/api/v1".freeze
     TOKEN_URL = BASE_URL + "/auth".freeze
     COUNTERS_URL = BASE_URL + "/counters".freeze
+    MACHINES_URL = BASE_URL + "/machines".freeze
+    CAN_DATA_URL = BASE_URL + "/can_data".freeze
 
     authenticate_with :check do
       parameter :email
       parameter :password
     end
 
-    calls :get_token, :fetch_all_counters
+    calls :get_token, :fetch_all_counters, :fetch_j1939_bus, :fetch_geolocation, :fetch_fields_worked
 
     # Get token with login and password
+    #DOC https://doc.samsys.io/#api-Authentication-Authentication
     def get_token
       integration = fetch
-
-      # for testing
-      # call = RestClient.post url: TOKEN_URL, {email: email, password: password}
-      # token = JSON(call.body).deep_symbolize_keys[:jwt]
-
       payload = {"email": integration.parameters['email'], "password": integration.parameters['password']}
       post_json(TOKEN_URL, payload) do |r|
+        r.error :api_down unless r.body.include? 'ok'
         r.success do
           list = JSON(r.body).deep_symbolize_keys
-          r.error :api_down unless r.body.include? 'ok'
+          integration.parameters['token'] = list[:jwt]
+          integration.save!
+          Rails.logger.info 'CHECKED'.green
         end
       end
     end
 
     # Get all counters
+    # DOC https://doc.samsys.io/#api-Counters-Get_all_counters_of_a_user
     def fetch_all_counters
       integration = fetch
-      # Grab token
-      token = JSON(get_token.body).deep_symbolize_keys[:jwt]
-
+      # Get token
+      if integration.parameters['token'].blank?
+        get_token
+      end
       # for testing
       # call = RestClient::Request.execute(method: :get, url: COUNTERS_URL, headers: {Authorization: "JWT #{token}"})
       # counters = JSON.parse(call.body).map{|p| p.deep_symbolize_keys}
 
       # Call API
-      get_json(COUNTERS_URL, 'Authorization' => "JWT #{token}") do |r|
+      get_json(COUNTERS_URL, 'Authorization' => "JWT #{integration.parameters['token']}") do |r|
         r.success do
           list = JSON(r.body).map{|p| p.deep_symbolize_keys}
+        end
+
+        r.redirect do
+          Rails.logger.info '*sigh*'.yellow
+        end
+
+        r.error do
+          Rails.logger.info 'What the fuck brah?'.red
+          Rails.logger.info 'Token_missing'.red if token.blank?
+        end
+      end
+    end
+
+    # Get J1939 Data of a machine
+    # DOC https://doc.samsys.io/#api-Machines-A_machine_j1939_data
+    # https://app.samsys.io/api/v1/machines/:id_machine/j1939_data
+    def fetch_j1939_bus(machine_id)
+      integration = fetch
+
+      # Get token
+      if integration.parameters['token'].blank?
+        get_token
+      end
+
+      # Call API
+      get_json("#{MACHINES_URL}/#{machine_id}/j1939_data", 'Authorization' => "JWT #{integration.parameters['token']}") do |r|
+        r.success do
+          list = JSON(r.body).deep_symbolize_keys
+        end
+
+        r.redirect do
+          Rails.logger.info '*sigh*'.yellow
+        end
+
+        r.error do
+          Rails.logger.info 'What the fuck brah?'.red
+        end
+      end
+    end
+
+    #Get last geolocation of a machine
+    # DOC https://doc.samsys.io/#api-Machines-A_machine_geolocation
+    # https://app.samsys.io/api/v1/machines/:id_machine/geolocation
+    def fetch_geolocation(machine_id)
+      integration = fetch
+
+      # Get token
+      if integration.parameters['token'].blank?
+        get_token
+      end
+
+      # Call API
+      get_json("#{MACHINES_URL}/#{machine_id}/geolocation", 'Authorization' => "JWT #{integration.parameters['token']}") do |r|
+        r.success do
+          list = JSON(r.body).deep_symbolize_keys
+        end
+
+        r.redirect do
+          Rails.logger.info '*sigh*'.yellow
+        end
+
+        r.error do
+          Rails.logger.info 'What the fuck brah?'.red
+        end
+      end
+    end
+
+    # Get fields work of a machine
+    # DOC https://doc.samsys.io/#api-Machines-A_machine_fields_worked_statistics
+    # https://app.samsys.io/api/v1/machines/:id_machine/statistics/fields_worked?start_date=:start_date&end_date=:end_date
+    def fetch_fields_worked(machine_id)
+      integration = fetch
+
+      # Get token
+      if integration.parameters['token'].blank?
+        get_token
+      end
+      stopped_on = Time.now.strftime("%FT%TZ")
+      started_on = (Time.now - 30.days).strftime("%FT%TZ")
+      puts "#{MACHINES_URL}/#{machine_id}/statistics/fields_worked?start_date=#{started_on}&end_date=#{stopped_on}".inspect.green
+      # Call API
+      get_json("#{MACHINES_URL}/#{machine_id}/statistics/fields_worked?start_date=#{started_on}&end_date=#{stopped_on}", 'Authorization' => "JWT #{integration.parameters['token']}") do |r|
+        r.success do
+          list = JSON(r.body).deep_symbolize_keys
+        end
+
+        r.redirect do
+          Rails.logger.info '*sigh*'.yellow
+        end
+
+        r.error do
+          list = JSON(r.body).deep_symbolize_keys
+        end
+      end
+    end
+
+    # Get CAN Data (ISOBUS) of a machine
+    # DOC https://doc.samsys.io/#api-Can_data-Get_historical_can_data
+    # https://app.samsys.io/api/v1/can_data?id_counter=:id_counter&id_counter=:id_counter&start_date=:start_date&end_date=:end_date&filter=:filter&filter=:filter
+    def fetch_can_bus(counter_ids, started_on, stopped_on, filter)
+      integration = fetch
+
+      # Get token
+      if integration.parameters['token'].blank?
+        get_token
+      end
+
+      # Build params (date in iso8601(3))
+      params = {
+                id_counter: counter_ids,
+                start_date: started_on.iso8601(3),
+                end_date: stopped_on.iso8601(3),
+                filter: filter
+      }
+
+      # Call API
+      get_json(CAN_DATA_URL, 'Authorization' => "JWT #{integration.parameters['token']}", params => params) do |r|
+        r.success do
+          list = JSON(r.body).deep_symbolize_keys
         end
 
         r.redirect do
@@ -74,14 +196,20 @@ module Samsys
     end
 
     # Check if the API is up
-    # TODO where to store token ?
+    # https://doc.samsys.io/#api-Authentication-Authentication
     def check(integration = nil)
       integration = fetch integration
+      puts integration.inspect.red
       payload = {"email": integration.parameters['email'], "password": integration.parameters['password']}
       post_json(TOKEN_URL, payload) do |r|
         r.success do
-          Rails.logger.info 'CHECKED'.green
-          r.error :api_down unless r.body.include? 'ok'
+          list = JSON(r.body).deep_symbolize_keys
+          if list[:status] == 'ok'
+            puts "check success".inspect.green
+            Rails.logger.info 'CHECKED'.green
+          end
+          r.error :wrong_password if list[:status] == '401'
+          r.error :no_account_exist if list[:status] == '404'
         end
       end
     end
--- app/jobs/samsys_fetch_update_create_job.rb
+++ app/jobs/samsys_fetch_update_create_job.rb
@@ -1,70 +1,236 @@
 class SamsysFetchUpdateCreateJob < ActiveJob::Base
   queue_as :default
+  include Rails.application.routes.url_helpers
 
-  def perform
+  # set default creation date older because we have no date for machine
+  DEFAULT_BORN_AT = Time.new(2010, 1, 1, 10, 0, 0, '+00:00')
 
-    # transcode Samsys machine type in Ekylibre machine nature
-    transcode_machine_type = {
-                            "Micro tracteur" => :tractor, "Tracteur agricole" => :tractor,
-                            "Tracteur de pente" => :tractor, "Tracteur enjambeur" => :tractor,
-                            "Tracteur forestier" => :tractor, "Tracteur fruitier" => :tractor,
-                            "Tracteur vigneron" => :tractor, "Unimog" => :tractor
+  # transcode Samsys machine type in Ekylibre machine nature
+  TRANSCODE_MACHINE_TYPE = {
+                          "micro tracteur" => :tractor, "tracteur agricole" => :tractor,
+                          "tracteur de pente" => :tractor, "tracteur enjambeur" => :tractor,
+                          "tracteur forestier" => :tractor, "tracteur fruitier" => :tractor,
+                          "tracteur vigneron" => :tractor, "unimog" => :tractor
+                            }.freeze
+
+  MACHINE_CUSTOM_FIELDS = {
+                          "model" => {name: "Modèle", customized_type: "Equipment", options: {column_name: "model_name"}},
+                          "brand" => {name: "Marque", customized_type: "Equipment", options: {column_name: "brand_name"}}
                           }.freeze
 
-    #TODO call get_token method here to avoid multiple call of get_token during one session
-
-    # Get all counter for a user
-    # https://doc.samsys.io/#api-Counters-Get_all_counters_of_a_user
-    Samsys::SamsysIntegration.fetch_all_counters.execute do |c|
-      c.success do |list|
-        list.map do |counter|
-
-          # puts counter.inspect.green
-          # counter attributes
-          # counter[:id]
-          # counter[:v_bat]
-          # counter[:v_ext]
-          # counter[:owner] {}
-          # counter[:association] {} --> machine {}
-
-          sensor = Sensor.find_or_create_by(
-            vendor_euid: :samsys,
-            model_euid: :samsys,
-            euid: counter[:id],
-            name: counter[:id],
-            retrieval_mode: :integration
-          )
-          sensor.update!(
-            battery_level: counter[:v_bat],
-            last_transmission_at: Time.now
-          )
-
-          # Find or create an equipment corresponding to the sensor
-
-          if counter[:association].any? && counter[:association][:machine].any?
-            counter[:association][:machine].each do |machine|
-              # Find or create an equipement corresponding to the machine
-              # machine[:id]
-              # machine[:name]
-              # machine[:brand]
-              # machine[:machine_type]
-              puts machine.inspect.yellow
+  # transcode Samsys machine indicators in Ekylibre machine indicators
+  MACHINE_INDICATORS = {
+                          :engine_total_hours_of_operation => {indicator: :hour_counter, unit: :hour},
+                          :fuel_level => {indicator: :fuel_level, unit: :percent}
+                          }.freeze
 
 
+  def perform
+    begin
+      # create custom field for all equipement if not exist
+      MACHINE_CUSTOM_FIELDS.each do |key, value|
+        unless cf = CustomField.find_by_name(value[:name])
+          create_custom_field_for_machine(value[:name], value[:customized_type], value[:options])
+        end
+      end
 
-              # Link the sensor to the machine
-              # counter[:association][:id]
-              # counter[:association][:start_date]
-              # counter[:association][:end_date]
+      # Get all counter for a user
+      # https://doc.samsys.io/#api-Counters-Get_all_counters_of_a_user
+      Samsys::SamsysIntegration.fetch_all_counters.execute do |c|
+        c.success do |list|
+          list.map do |counter|
+            # puts counter.inspect.green
+            # counter attributes
+            # counter[:id]
+            # counter[:v_bat]
+            # counter[:v_ext]
+            # counter[:owner] {}
+            # counter[:association] {} --> machine {}
 
+            # NOTE : in Ekylibre model
+            # sensor has_one sensor_equipment
+            # sensor_equipment has_many localisations (in equipment)
 
+            sensor = Sensor.find_or_create_by(
+              vendor_euid: :samsys,
+              model_euid: :samsys,
+              euid: counter[:id],
+              name: counter[:id],
+              retrieval_mode: :integration
+            )
+            sensor.update!(
+              battery_level: counter[:v_bat],
+              last_transmission_at: Time.now
+            )
+
+            # find_or_create_sensor_equipment
+            sensor_equipment = find_or_create_sensor_equipment(sensor, counter, c.id)
+
+            # link the equipment to sensor
+            sensor.update!(product_id: sensor_equipment.id) if sensor_equipment
+
+            if counter[:association].any? && counter[:association][:machine].any? && sensor_equipment
+              counter[:association][:machine].each do |machine|
+                # Find or create an equipement corresponding to the machine
+                find_or_create_machine_equipment(machine, counter, sensor_equipment, c.id)
+              end
+            end
+
+          end
+        end
+      end
+    rescue StandardError => error
+      Rails.logger.error $!
+      Rails.logger.error $!.backtrace.join("\n")
+      ExceptionNotifier.notify_exception($!, data: { message: error })
+    end
+  end
+
+  private
+
+  def find_or_create_sensor_equipment(sensor, counter, call_id)
+    # Find or create the owner
+    if counter[:owner][:type_cluster] == "farm"
+      owner = Entity.of_company
+    elsif counter[:owner][:type_cluster] != "farm"
+      #TODO create the entity
+    end
+
+    # Find the variant corresponding to geolocalisation sensor
+    #TODO add sensor to Lexicon and replace electric_pruning by good reference
+    sensor_variant = ProductNatureVariant.import_from_lexicon(:flatbed_trailer)
+
+    # Find or create an equipment corresponding to the sensor
+    # create the equipment
+    sensor_equipments = Equipment.where('providers ->> ? = ?', 'samsys', counter[:id])
+    if sensor_equipments.any?
+      sensor_equipment = sensor_equipments.first
+    else
+      sensor_equipment = Equipment.create!(
+        variant_id: sensor_variant.id,
+        name: counter[:id],
+        initial_born_at: sensor.created_at,
+        initial_population: 1,
+        initial_owner: owner,
+        work_number: "SAMSYS_" + counter[:id].to_s,
+        providers: {'samsys' => counter[:id], 'call_id' => call_id}
+      )
+    end
+    sensor_equipment
+  end
+
+  def find_or_create_machine_equipment(machine, counter, sensor_equipment, call_id)
+    # machine[:id]
+    # machine[:name]
+    # machine[:brand]
+    # machine[:machine_type]
+    puts machine.inspect.yellow
+
+    # Find or create the owner
+    if counter[:owner][:type_cluster] == "farm"
+      owner = Entity.of_company
+    elsif counter[:owner][:type_cluster] != "farm"
+      #TODO create the entity
+    end
+
+    variant_to_find = TRANSCODE_MACHINE_TYPE[machine[:machine_type].downcase]
+    if variant_to_find
+      equipment_variant = ProductNatureVariant.import_from_lexicon(variant_to_find)
+    else
+      equipment_variant = ProductNatureVariant.import_from_lexicon(:tractor)
+    end
+
+    machine_equipments = Equipment.where('providers ->> ? = ?', 'samsys', machine[:id])
+    if machine_equipments.any?
+      machine_equipment = machine_equipments.first
+    else
+      machine_equipment = Equipment.create!(
+        variant_id: equipment_variant.id,
+        name: machine[:name],
+        initial_born_at: DEFAULT_BORN_AT,
+        initial_population: 1,
+        initial_owner: owner,
+        work_number: "SAMSYS_" + machine[:id].to_s,
+        providers: {'samsys' => machine[:id], 'call_id' => call_id}
+      )
+    end
+    cf = CustomField.find_by(column_name: "brand_name")
+    machine_equipment.set_custom_value(cf, machine[:brand])
+
+    # Get indicator from J1939 bus for a machine
+    Samsys::SamsysIntegration.fetch_j1939_bus(machine[:id]).execute do |c|
+      c.success do |j1939_indicators|
+        if j1939_indicators[:t] != nil
+          [:engine_total_hours_of_operation, :fuel_level].each do |j1939_ind|
+            transcoded_indicator = MACHINE_INDICATORS[j1939_ind]
+            if j1939_indicators[j1939_ind] != nil && machine_equipment.variant.has_indicator?(transcoded_indicator[:indicator])
+              puts transcoded_indicator.inspect.yellow
+              machine_equipment.read!(transcoded_indicator[:indicator], j1939_indicators[j1939_ind].in(transcoded_indicator[:unit]), at: j1939_indicators[:t], force: true)
             end
           end
-
         end
       end
     end
 
+    # Get geolocation for a machine
+    Samsys::SamsysIntegration.fetch_geolocation(machine[:id]).execute do |c|
+      c.success do |geolocation|
+        if machine_equipment.variant.has_indicator?(:geolocation)
+          lat_lon = geolocation[:geometry][:coordinates]
+          point = ::Charta.new_point(lat_lon[0], lat_lon[1]).to_ewkt
+          read_at = geolocation[:properties][:t]
+          machine_equipment.read!(:geolocation, point, at: read_at, force: true) if point && read_at
+        end
+      end
+    end
 
+    # Get fields work of a machine
+    Samsys::SamsysIntegration.fetch_fields_worked(machine[:id]).execute do |c|
+      c.success do |fields|
+        puts fields.inspect.green
+      end
+      c.error do |e|
+        puts e.inspect.red
+      end
+    end
+
+    # Link the sensor to the machine
+    # counter[:association][:id]
+    # counter[:association][:start_date]
+    # counter[:association][:end_date]
+    if sensor_equipment && machine_equipment
+      ProductLink.find_or_create_by(
+        product_id: sensor_equipment.id,
+        linked_id: machine_equipment.id,
+        nature: "sensor",
+        started_at: counter[:association][:start_date]
+      )
+    end
   end
+
+  def create_custom_field_for_machine(name, customized_type, options = {})
+    # create custom field
+    cf = CustomField.create!(name: name, customized_type: customized_type,
+                                   nature: options[:nature] || :text,
+                                   column_name: options[:column_name])
+    # create custom field choice if nature is choice
+    if cf.choice? && options[:choices]
+      options[:choices].each do |value, label|
+        cf.choices.create!(name: label, value: value)
+      end
+    end
+  end
+
+  def error_notification_params(error)
+    {
+      message: 'error_during_samsys_api_call',
+      level: :error,
+      target_type: '',
+      target_url: '',
+      interpolations: {
+        error_message: error
+      }
+    }
+  end
+
 end
