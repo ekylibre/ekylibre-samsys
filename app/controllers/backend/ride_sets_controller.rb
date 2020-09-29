9730e11715e43441b1f127a914b1ca3c46b43149
Guillaume <groche@ekylibre.com>
2020-09-29 12:12:00 +0200

    Clean indentation on ride_sets_controller

--- app/controllers/backend/ride_sets_controller.rb
+++ app/controllers/backend/ride_sets_controller.rb
@@ -5,7 +5,7 @@ module Backend
 
     unroll
 
-  	 list do |t|
+    list do |t|
       t.column :number, url: true
       t.column :nature
       t.column :started_at
@@ -14,7 +14,7 @@ module Backend
       t.column :sleep_count
       t.column :sleep_duration_iso
       t.column :provider_vendor
-  	 end
+    end
 
     list(:rides, model: :ride, conditions: { ride_set_id: 'params[:id]'.c }) do |t|
       t.column :number, url: true
