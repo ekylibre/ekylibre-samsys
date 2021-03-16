f9a0f0be3ccfb7ac46dabe32b64c5c6d4bb52dd6
David JOULIN <djoulin@ekylibre.com>
2021-03-16 09:54:38 +0100

    Put equipment view into ekyviti/core

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,9 @@ Rails.application.routes.draw do
     get :list, on: :collection
   end
 
-  namespace :backend, only: :list_links do
-    resources :equipments do
-      member do
-        get :list_links
-      end
-    end
-  end
-
   namespace :backend do
     resources :rides, concerns: %i[list], only: %i[index show destroy]
   end
-
 
   namespace :backend do
     resources :ride_sets, concerns: %i[list], only: %i[index show destroy] do
