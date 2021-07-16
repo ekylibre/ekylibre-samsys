ef0e654ba985622ca6745467e9748b7557c32e7e
Guillaume <groche@ekylibre.com>
2021-07-16 16:13:42 +0200

    Updated test

--- test/integrations/samsys/handlers/ride_sets_test.rb
+++ test/integrations/samsys/handlers/ride_sets_test.rb
@@ -14,13 +14,11 @@ class HandlersRideSetsTest < ::Ekylibre::Testing::ApplicationTestCase::WithFixtu
       machine_custom_fields.bulk_find_or_create
 
       stopped_on = Time.now
-      ride_sets = ::Samsys::Handlers::RideSets.new(stopped_on: stopped_on)
-      ride_sets.bulk_find_or_create
-
-      assert_equal Ride.last.equipment, Equipment.last, "Must create machine equipment"
-      assert_equal RideSet.present?, true, 'Must create RideSet'
-      assert_equal Ride.present?, true, 'Must create Ride'
-      assert_equal Crumb.present?, true, 'Must create Crumb'
+      assert_difference 'RideSet.count', 3 do
+        ::Samsys::Handlers::RideSets.new(stopped_on: stopped_on).bulk_find_or_create
+      end
     end
+    
   end
 end
+ 
